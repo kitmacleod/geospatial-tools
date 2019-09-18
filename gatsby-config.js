5915caf58b66e79fea09ea6f451292c0d765f0d7
Kit Macleod <kitmacleod@gmail.com>
2019-09-18 12:00:27 +0100

    adding Contentful cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,14 @@ module.exports = {
     author: "Andrew Mead",
   },
   plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
     "gatsby-plugin-sass",
     "gatsby-plugin-netlify-cms",
     {
