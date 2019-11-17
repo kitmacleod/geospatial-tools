a330e5a50c4ad26b0ec0c5a4c74f534af4eade14
Kit Macleod <kitmacleod@gmail.com>
2019-11-17 13:38:17 +0000

    Formatted tools page based on blog page

--- src/pages/tools.js
+++ src/pages/tools.js
@@ -1,16 +1,55 @@
 import React from "react"
+import { Link, graphql, useStaticQuery } from "gatsby"
+
 import Layout from "../components/layout"
+import toolsStyles from "./tools.module.scss"
 import Head from "../components/head"
 
 const ToolsPage = () => {
+  const data = useStaticQuery(graphql`
+  query {
+    allMarkdownRemark {
+      edges {
+        node {
+          fields {
+            githubData {
+              repository {
+                stargazers {
+                  totalCount
+                }
+                name
+              }
+            }
+          }
+          frontmatter {
+            title
+            slug
+            date
+          }
+        }
+      }
+    }
+  }
+  `)
+
   return (
-      <Layout>
-        <Head title="Tools" />
-        <h1>Tools</h1>
-        <p>This is a place holder.</p>
-      
-      </Layout>
- 
+    <Layout>
+      <Head title="Tools" />
+      <h1>Tools</h1>
+      <ol className={toolsStyles.posts}>
+        {data.allMarkdownRemark.edges.map(edge => {
+          return (
+            <li className={toolsStyles.post}>
+              <Link to={`/tools/${edge.node.frontmatter.slug}`}>
+                <h2>{edge.node.frontmatter.title}</h2>
+                <p>{edge.node.frontmatter.date}</p>
+                <p>{edge.node.fields.githubData.repository.stargazers.totalCount}</p>
+              </Link>
+            </li>
+          )
+        })}
+      </ol>
+    </Layout>
   )
 }
 
