8475d96d7cb1d94a035d982e58b23ae097a26e40
Kit Macleod <Kit.Macleod@hutton.ac.uk>
2020-04-13 09:41:22 +0100

    Update Contact page Twitter account

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,8 +10,8 @@ const ContactPage = () => {
       <h1>Contact</h1>
       <p>
         The best way to reach me is via{" "}
-        <a href="https://twitter.com/@andrew_j_mead" target="_blank">
-          @andrew_j_mead
+        <a href="https://twitter.com/@kitmacleod" target="_blank">
+          @kitmacleod
         </a>{" "}
         on Twitter!
       </p>
